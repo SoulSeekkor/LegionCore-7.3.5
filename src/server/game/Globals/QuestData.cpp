0558c5961926912759fc76e2a78d23e7265f8162
SoulSeekkor <soulseekkor@live.com>
2024-11-30 21:23:35 -0600

    Quick test.

--- src/server/game/Globals/QuestData.cpp
+++ src/server/game/Globals/QuestData.cpp
@@ -538,6 +538,7 @@ void QuestDataStoreMgr::LoadQuests()
                 newQuest->MinLevel = levelEntry->minLevel;
                 newQuest->MaxLevel = levelEntry->maxLevel;
                 newQuest->MaxScalingLevel = levelEntry->maxLevel;
+                newQuest->Expansion = 0;
                 TC_LOG_ERROR(LOG_FILTER_SERVER_LOADING, "LoadQuests() >> Adjusted min level to %u and max level to %u for quest id %u in zone %u.", newQuest->MinLevel, newQuest->MaxScalingLevel, newQuest->GetQuestId(), questEntry->zoneId);
             }
         }
